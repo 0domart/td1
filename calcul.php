eaf46b269db871d436571c6738f67a8cb5e2595b
g18020232 <julien.godest@etu.univ-amu.fr>
2019-09-16 16:49:37 +0200

    exo1

--- calcul.php
+++ calcul.php
@@ -27,8 +27,8 @@ elseif('+'== $op)
 }
 else
 {
-    echo\'<br/><strong>opérateur\'.$op .\'nongéré</strong>\';
+    echo'<br/><strong>opérateur'.$op.'nongéré</strong>';
 }
-if (isset($res)) echo $op1. $op . $op2 . \'=\' . $res;
+if (isset($res)) echo $op1. $op . $op2 . '=' . $res;
+end_page();
 ?>
-
