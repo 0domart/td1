3dfebdcfa8523bcde9e05203c516adcbf761f4c5
g18020232 <julien.godest@etu.univ-amu.fr>
2019-09-16 14:17:08 +0200

    exo1

---
+++ td1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <p>Titre</p>p>
+</head>
+<body>
+<?php
+echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
+echo 'C\'est pas mal non ? <br/>';
+?>
+</body>
+</html
