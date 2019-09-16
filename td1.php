43f5923341aeb6bfee4f397db6d011de1efea1c4
g18020232 <julien.godest@etu.univ-amu.fr>
2019-09-16 14:54:49 +0200

    exo1

--- td1.php
+++ td1.php
@@ -1,12 +1,3 @@
 <?php
-function start_page($title)
-{
-    echo ' <!DOCTYPE html> <html
-lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body><hr/><br/><strong>Test</strong><br/><hr/></body>' . PHP_EOL
-    ;
-};
-?>
-
-<?php
-start_page('test');
+echo date('l F d, Y');
 ?>
