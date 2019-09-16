935bab5aa3b588d3bf039146880f508207778a0e
g18020232 <julien.godest@etu.univ-amu.fr>
2019-09-16 15:51:20 +0200

    exo1

--- td1.php
+++ td1.php
@@ -11,14 +11,14 @@ lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body><hr/><br/><s
 <?php
 start_page('Calculatrice');
 ?>
-<form action="calcul.php" method="post">
+<form>
     <input type="text" name="op1" /> <br/>
     <input type="text" name="op2" /> <br/>
     <input checked="checked" type="radio" name="op" value="*"/>*<br/>
     <input type="radio" name="op" value="+"/>+<br/>
     <input type="radio" name="op" value="-"/>-<br/>
     <input type="radio" name="op" value="/" />/<br/>
-    <input type="radio" name="op3" value="="/>=<br/>
+    <input action="calcul.php" method="post" type="radio" name="op3" value="="/>=<br/>
     <input type="radio" name="op4"value="clear"/>clear<br/>
  </form>
 
