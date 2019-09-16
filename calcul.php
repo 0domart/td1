d0a9b9641466e3b579ecc561bb34690e4cccd24c
g18020232 <julien.godest@etu.univ-amu.fr>
2019-09-16 16:45:14 +0200

    exo1

--- calcul.php
+++ calcul.php
@@ -11,13 +11,12 @@ lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body><hr/><br/><s
 start_page('CalculatriceResultat');
 ?>
 
-
 <?php
 $op1 = $_POST['op1'];
 $op2 = $_POST['op2'];
 $op = $_GET['op'];
 
-echo $op1  .  $op2;
+echo $op1. $op2;
 if('*'== $op)
 {
     $res = $op1 * $op2;
