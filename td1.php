efd6ab30fa1d8ad8657d7791d9b04485bfb4cafc
g18020232 <julien.godest@etu.univ-amu.fr>
2019-09-16 15:31:28 +0200

    exo1

--- td1.php
+++ td1.php
@@ -18,8 +18,8 @@ start_page('Calculatrice');
     <input type="radio" name="op" value="+"/>+<br/>
     <input type="radio" name="op" value="-"/>-<br/>
     <input type="radio" name="op" value="/"/>/<br/>
-    <input type="radio" name="submit" value=" = "/>/<br/>
-    <input type="radio" name="clear" value="clear"/>/<br/>
+    <input type="radio" name="submit"/>=<br/>
+    <input type="radio" name="clear" />clear<br/>                                                                                                                                                                                                             "/>/<br/>
  </form>
 
 
