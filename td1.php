fb37633896dcd455333832b8a058986777cdd254
g18020232 <julien.godest@etu.univ-amu.fr>
2019-09-16 16:33:09 +0200

    exo1

--- td1.php
+++ td1.php
@@ -9,18 +9,20 @@ lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body><hr/><br/><s
 
 
 <?php
-start_page('Calculatrice');
-echo '<form action="calcul.php" method="post">
-        <input type="text" name="op1" /> <br/>
-        <input type="text" name="op2" /> <br/>
-        <input checked="checked" type="radio" name="op" value="*"/>*<br/>
-        <input checked="checked" type="radio" name="op" value="/"/>/<br/>
-        <input checked="checked" type="radio" name="op" value="-"/>-<br/>
-        <input checked="checked" type="radio" name="op" value="+"/>+<br/>
-        <input type="radio" name="op3" value="="/>=<br/>
-        <input type="radio" name="op4"value="clear"/>clear<br/>
-    </form>
-';
+start_page("calculatrice");
+$operateurs = '*+-/';
+echo '
+    <form action = "calcul.php" method = "get">
+    <input type = "text" name = "op1" ><br>
+    <input type = "text" name = "op2" ><br>';
+for($cpt=0;$cpt<=3;++$cpt)
+{
+    echo '<input ';
+    if($cpt==0)
+    {
+        echo'checked = "checked"';
+    }
+    echo'type="submit" name="action" value="'.$operateurs[$cpt].'"/>'.$operateurs[$cpt].'<br/>
+        '."\n";}
+echo '</form>';
 ?>
-
-
