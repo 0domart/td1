eeb9da8f47a9bcd0e9a68c8b039a0556c8645d1c
g18020232 <julien.godest@etu.univ-amu.fr>
2019-09-16 16:12:36 +0200

    exo1

--- calcul.php
+++ calcul.php
@@ -1,4 +1,18 @@
 <?php
+function start_page($title)
+{
+    echo ' <!DOCTYPE html> <html
+lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body><hr/><br/><strong>Calculatrice</strong><br/><br><hr/></body>' . PHP_EOL
+    ;
+};
+?>
+
+<?php
+start_page('CalculatriceResultat');
+?>
+
+
+<?php
 $op1 = $_POST['op1'];
 $op2 = $_POST['op2'];
 $op = $_POST['op'];
