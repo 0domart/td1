d44c49528c299c5c49bff134c4dd30f79b671763
g18020232 <julien.godest@etu.univ-amu.fr>
2019-09-16 14:47:35 +0200

    exo1

--- td1.php
+++ td1.php
@@ -1,20 +1,12 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-    <p>Titre</p>
-</head>
-<body>
 <?php
-echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
-echo 'C\'est pas mal non ? <br/>';
-$var1 = 6;
-$var2 = 1.3;
-$var3 = 'Variable 3';
-echo $var3;
-echo '<br>';
-echo $var1;
-echo '<br>';
-echo $var2;
+function start_page($title)
+{
+    echo ' <!DOCTYPE html> <html
+lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL
+    ;
+};
 ?>
-</body>
-</html
+
+<?php
+start_page('titre');
+?>
