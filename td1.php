55dbe4e74bdfd4c41591f140f397a8dda1cec8b4
g18020232 <julien.godest@etu.univ-amu.fr>
2019-09-16 14:57:24 +0200

    exo1

--- td1.php
+++ td1.php
@@ -5,7 +5,15 @@ echo date('l F d, Y');
 <?php
 $jour = date('l F d, Y');
 ?>
-
+<br>
+<?php
+echo $jour;
+?>
+<br>
+<?php
+$jour = date('d/m/Y', strtotime('2020-04-01'));
+?>
+<br>
 <?php
 echo $jour;
 ?>
