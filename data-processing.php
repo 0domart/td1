67786b904ae1712040a6dee47a5de923afb47a0c
g18020232 <julien.godest@etu.univ-amu.fr>
2019-09-25 10:47:38 +0200

    exo1

--- data-processing.php
+++ data-processing.php
@@ -19,7 +19,8 @@ if ($action == 'mailer') {
     $pays = $_POST['pays'];
     $cgu = $_POST['cgu'];
 
-    $message = 'Identifiant: ' . $id . PHP_EOL . "\t";
+    $message = 'Identifiant: ' . $id . PHP_EOL ;
+    $message .= "\t";
     $message .= 'Sexe : ' . $sexe . PHP_EOL;
     $message .= 'E-mail : ' . $email . PHP_EOL;
     $message .= 'Password : ' . $mdp . PHP_EOL;
