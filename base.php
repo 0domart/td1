97187ac03b7587425c626633b9014439dbcb2c50
g18020232 <julien.godest@etu.univ-amu.fr>
2019-09-25 11:45:53 +0200

    exo1

---
+++ base.php
@@ -0,0 +1,37 @@
+<?php
+$dbLink = mysqli_connect(mysql-odomart.alwaysdata.net, odomart_tp2, julien69960)
+or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
+?>
+
+<?php
+mysqli_select_db($dbLink , odomart_td2)
+or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink)
+);
+?>
+
+
+<?php
+$query = 'SELECT id, email, date FROM user';
+?>
+
+<?php
+if(!($dbResult = mysqli_query($dbLink, $query)))
+{
+    echo 'Erreur de requête<br/>';
+// Affiche le type d'erreur.
+    echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+// Affiche la requête envoyée.
+    echo 'Requête : ' . $query . '<br/>';
+    exit();
+}
+?>
+
+while($dbRow = mysqli_fetch_assoc($dbResult))
+{
+echo $dbRow['id'] . '<br/>';
+echo $dbRow['mail'] . '<br/>';
+echo date('d.m.Y', strtotime($dbRow['date']));
+echo '<br/><br/>';
+}
+?>
+<a href="Formulaire.php"><button>Revenir</button></a>
