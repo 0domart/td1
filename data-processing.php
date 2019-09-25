eaab58aa23d7621f10c7d95fa58101d0916ee512
g18020232 <julien.godest@etu.univ-amu.fr>
2019-09-25 10:05:35 +0200

    exo1

--- data-processing.php
+++ data-processing.php
@@ -19,9 +19,10 @@ if ($action == 'mailer') {
 
     $message = 'Voici vos identifiants d\'inscription :' . PHP_EOL;
     $message .= 'Email : ' . $email . PHP_EOL;
-    $message .= 'Mot de passe : ' . PHP_EOL . $password;
-    echo $message
+    $message .= 'Mot de passe : ' . PHP_EOL . $mdp;
+    echo $message;
 
 }
-} else {
+else {
     echo '<br/><strong>Bouton non géré !</strong><br/>';
+    }
