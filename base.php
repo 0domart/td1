3a6669e3f7bf4f1a576ba01358ca77003e52aa76
g18020232 <julien.godest@etu.univ-amu.fr>
2019-09-30 09:07:55 +0200

    exo1

--- base.php
+++ base.php
@@ -1,28 +1,24 @@
 <?php
-$dbLink = mysqli_connect('mysql-odomart.alwaysdata.net', 'odomart_tp2', 'julien69960')
+$dbLink = mysqli_connect('mysql-odomart.alwaysdata.net', 'odomart', 'julien69960')
 or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
-
-mysqli_select_db($dbLink , odomart_td2)
-or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink)
-);
-
+mysqli_select_db($dbLink , 'steffen_td2')
+or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
 $query = 'SELECT id, email, date FROM user';
-
 if(!($dbResult = mysqli_query($dbLink, $query)))
 {
     echo 'Erreur de requête<br/>';
-// Affiche le type d'erreur.
+    // Affiche le type d'erreur.
     echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
-// Affiche la requête envoyée.
+    // Affiche la requête envoyée.
     echo 'Requête : ' . $query . '<br/>';
     exit();
 }
-
 while($dbRow = mysqli_fetch_assoc($dbResult))
 {
     echo $dbRow['id'] . '<br/>';
     echo $dbRow['mail'] . '<br/>';
+    echo date('d.m.Y', strtotime($dbRow['date']));
     echo '<br/><br/>';
 }
 ?>
-<a href="Formulaire.php"><button>Revenir</button></a>
+<a href="index.php"><button>Revenir</button></a>
