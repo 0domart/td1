eedb175b5a86e407bd047913ba93b7015be12c65
g18020232 <julien.godest@etu.univ-amu.fr>
2019-10-02 08:35:12 +0200

    exo1

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ include 'utils.inc.php';
 
 <?php start_page("Login"); ?>
 
-<form action="test-pwd.php" method="post">
+<form action="test-pass.php" method="post">
     <input type="text" name="Login" placeholder="Login"><br>
     <input type="password" name="mdp" placeholder="Password"><br>
     <input name="action" type="submit" >
