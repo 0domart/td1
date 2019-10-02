f3d3881d2370c586365253ab5243f22f1de7ed28
g18020232 <julien.godest@etu.univ-amu.fr>
2019-10-02 08:45:53 +0200

    exo1

--- test-pass.php
+++ test-pass.php
@@ -8,9 +8,19 @@ $login = $_POST['Login'];
 $mdp = $_POST['mdp'];
 
 
-
-
-
+$dbLink = mysqli_connect('mysql-odomart.alwaysdata.net', 'odomart', 'julien69960')
+or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
+mysqli_select_db($dbLink, 'odomart_tp2')
+or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
+?>
+<?php
+$req = $dbLink->query("SELECT * FROM users WHERE identifiant = '" . $login . "' AND password = '" . $mdp. "'");
+if ($req->rowCount() == 0){
+    $_SESSION['errors'] = 'Identifiant ou mot de passe invalide !';
+    } else {
+    $_SESSION['success'] = "Vous êtes connecté ! ";
+    }
+    ?>
 
 
 <?php end_page(); ?>
