1a67c7ff0e21521816eb661fd45c465a4938af4d
g18020232 <julien.godest@etu.univ-amu.fr>
2019-10-03 14:56:49 +0200

    exo1

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ include 'utils.inc.php';
     <input type="text" name="login" placeholder="Login"><br>
     <input type="password" name="pwd" placeholder="Password"><br>
     <input type="text" name="sexe" placeholder="sxe"><br>
-    <input type="password" name="email" placeholder="mlail"><br>
+    <input type="text" name="email" placeholder="mlail"><br>
     <input type="text" name="tel" placeholder="tel"><br>
     <input name="action" type="submit" >
 </form>
