cd6bf87986f0e6182496849b7c21004dad8092df
g18020232 <julien.godest@etu.univ-amu.fr>
2019-10-10 09:28:00 +0200

    exo1

--- password.php
+++ password.php
@@ -7,8 +7,8 @@ $bdd = mysqli_select_db($dbLink , 'odomart_tp2');
 
 
 if (isset($POST['recup_submit'], $_POST['recup_mail'])) {
+    echo "aaa";
     if (!empty($POST['recup_mail'])) {
-        echo "aaa";
         $recup_mail = htmlspecialchars($POST['mail']);
         if (filter_var($recup_mail, FILTER_VALIDATE_EMAIL)) {
 
