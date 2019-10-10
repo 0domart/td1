d67924ac782f29da21b949493d61feb70830134d
g18020232 <julien.godest@etu.univ-amu.fr>
2019-10-10 09:32:42 +0200

    exo1

--- lostpassword.php
+++ lostpassword.php
@@ -8,4 +8,4 @@ include 'utils.inc.php';
         <input type="email" placeholder="Votre adresse mail" name="recup_mail"/><br/>
         <input type="submit" value="Valider" name="recup_submit"/>
     </form>
-<?php if(isset($error)) { echo '<span style="color:red">'.$error.'</span>'; } else { echo "dzadza"; } ?>
+<?php if(isset($error)) { echo '<span style="color:red">'.$error.'</span>'; } else { echo ""; } ?>
