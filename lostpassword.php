ab9da7b4a439b914f9b4934b674950b0dbb859af
g18020232 <julien.godest@etu.univ-amu.fr>
2019-10-10 09:18:29 +0200

    exo1

--- lostpassword.php
+++ lostpassword.php
@@ -4,6 +4,7 @@ include 'utils.inc.php';
 
 <?php start_page("Recup mdp"); ?>
 
+<?php { ?>
     <form method="post" action=""password.php">
         <input type="email" placeholder="Votre adresse mail" name="recup_mail"/><br/>
         <input type="submit" value="Valider" name="recup_submit"/>
