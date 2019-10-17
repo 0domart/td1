fb1c55a801f1564a714adc9a9b21350f9347d12b
g18020232 <julien.godest@etu.univ-amu.fr>
2019-10-17 09:13:05 +0200

    exo1

---
+++ mail.php
@@ -0,0 +1,20 @@
+
+<form class='modal-content' action='index.php?page=pswlost' method='post'>
+				    <div class='container'>
+				      <h1>Sign in </h1>
+					      <p>Rejoins nous</p>
+					      <hr>
+					      <label for='email'><b>Email</b></label>
+					      <input type='text' placeholder='Entrer Email' name='email' id='email' required>
+
+				      <p>Soit d'accord t'as pas le choix avec les :  <a href='#' style='color:dodgerblue'>Terms & Privacy</a>.</p>
+				      <div class='clearfix'>
+				        <button type='button' onclick=\"document . getElementById('id01') . style . display = 'none'\" class='cancelbtn'>Cancel</button>
+				        <button type='submit' id='button' name='new-password' value='Password Account' class='signupbtn'>Sign in</button>
+				        <p id='signup-form-message'>$pswlostFormMessage</p>
+				      </div>
+				    </div>
+				  </form>
+
+			</div>
+        </div>
